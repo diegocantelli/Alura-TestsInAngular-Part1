98485f22d8b443ac10b1fcc3e9bd730673bf8d4c
diegocantelli <diegocantelli19@gmail.com>
2024-02-14 10:44:48 -0300

    Testando a geracao de ids unicos atraves do metodo toBe

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -6,5 +6,16 @@ describe(UniqueIdService.name, () => {
     const service = new UniqueIdService();
     const id = service.generateUniqueIdWithPrefix('app');
     expect(id.startsWith('app')).toBeTrue();
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate multiple ids when called multiple times`, () => {
+    const service = new UniqueIdService();
+    const ids = new Set();
+
+    for (let i = 0; i < 50; i++) {
+      ids.add(service.generateUniqueIdWithPrefix('app-'));
+    }
+
+    expect(ids.size).toBe(50);
   })
 })
