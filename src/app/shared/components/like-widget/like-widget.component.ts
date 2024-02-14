bcd27c4c267e3678546df6179af7b2bfe86b8209
diegocantelli <diegocantelli19@gmail.com>
2024-02-14 16:00:05 -0300

    Criando o componente like-widget

---
+++ src/app/shared/components/like-widget/like-widget.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-like-widget',
+  templateUrl: 'like-widget.component.html',
+  styleUrls: ['like-widget.component.scss']
+})
+export class LikeWidgetComponent {
+
+}
---
+++ src/app/shared/components/like-widget/like-widget.module.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from "@angular/common";
+import { EventEmitter, Input, NgModule, Output } from "@angular/core";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { LikeWidgetComponent } from "./like-widget.component";
+
+@NgModule({
+  declarations: [LikeWidgetComponent],
+  imports: [CommonModule],
+  exports: [LikeWidgetComponent]
+})
+export class LikeWidgetModule {
+  @Output() public liked = new EventEmitter<void>();
+  @Input() public likes = 0;
+  @Input() public id = null;
+
+}
