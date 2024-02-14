f7a5b230c9e9a915afe87480a1340a0a4472e8cd
diegocantelli <diegocantelli19@gmail.com>
2024-02-14 16:09:46 -0300

    Implementando a api basica do like-widget component

--- src/app/shared/components/like-widget/like-widget.module.ts
+++ src/app/shared/components/like-widget/like-widget.module.ts
@@ -2,15 +2,15 @@ import { CommonModule } from "@angular/common";
 import { EventEmitter, Input, NgModule, Output } from "@angular/core";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { LikeWidgetComponent } from "./like-widget.component";
+import { UniqueIdService } from "../../services/unique-id/unique-id.service";
 
 @NgModule({
   declarations: [LikeWidgetComponent],
   imports: [CommonModule],
-  exports: [LikeWidgetComponent]
+  exports: [LikeWidgetComponent],
+  providers: [UniqueIdService]
 })
 export class LikeWidgetModule {
-  @Output() public liked = new EventEmitter<void>();
-  @Input() public likes = 0;
-  @Input() public id = null;
+
 
 }
