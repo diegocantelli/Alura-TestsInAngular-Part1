a66899fd277d7f70913f7966e46e6fa474ffee26
diegocantelli <diegocantelli19@gmail.com>
2024-02-17 19:20:03 -0300

    Utilizando o metodo detectChanges para dispara o ngOnInit

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,12 +1,14 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { LikeWidgetComponent } from "./like-widget.component";
+import { UniqueIdService } from "../../services/unique-id/unique-id.service";
+import { LikeWidgetModule } from "./like-widget.module";
 
 describe(LikeWidgetComponent.name, () => {
   let fixture: ComponentFixture<LikeWidgetComponent> = null;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({ //configura um modulo de testes para o componente em questao
-      declarations: [LikeWidgetComponent]
+      imports: [LikeWidgetModule]
     }).compileComponents() //compila o componente sendo testado, resolvendo seu template, css e etc
 
     fixture = TestBed.createComponent(LikeWidgetComponent); //Este metodo retorna um ComponentFixture<LikeWidgetComponent>
@@ -15,5 +17,11 @@ describe(LikeWidgetComponent.name, () => {
   it('It should create component', () => {
     const instance = fixture.componentInstance;
     expect(instance).toBeTruthy();
+  })
+
+  it('Should auto-generate ID when id input property is missing', () => {
+    const instance = fixture.componentInstance;
+    fixture.detectChanges(); //dispara o mecanismo de change detection e dispara o lifecicle hook ngOnInit do componente
+    expect(instance.id).toBeTruthy();
   })
 })
