78670c76528ad85af50b343cc9d00438cb074847
diegocantelli <diegocantelli19@gmail.com>
2024-02-17 17:24:00 -0300

    Utilizando TestBed.createComponent para criar uma fixture do componente a ser testado

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,14 +1,19 @@
-import { TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { LikeWidgetComponent } from "./like-widget.component";
 
 describe(LikeWidgetComponent.name, () => {
-  let component: LikeWidgetComponent = null;
+  let fixture: ComponentFixture<LikeWidgetComponent> = null;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({ //configura um modulo de testes para o componente em questao
       declarations: [LikeWidgetComponent]
     }).compileComponents() //compila o componente sendo testado, resolvendo seu template, css e etc
+
+    fixture = TestBed.createComponent(LikeWidgetComponent); //Este metodo retorna um ComponentFixture<LikeWidgetComponent>
   })
 
-  it('', () => {})
+  it('It should create component', () => {
+    const instance = fixture.componentInstance;
+    expect(instance).toBeTruthy();
+  })
 })
