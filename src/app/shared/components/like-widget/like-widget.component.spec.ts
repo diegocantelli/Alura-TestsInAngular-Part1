4bb4ca209a785e6e4c880ef9059e2ca3a2d84756
diegocantelli <diegocantelli19@gmail.com>
2024-02-17 19:25:55 -0300

    Refatorando o teste

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -5,23 +5,29 @@ import { LikeWidgetModule } from "./like-widget.module";
 
 describe(LikeWidgetComponent.name, () => {
   let fixture: ComponentFixture<LikeWidgetComponent> = null;
-
+  let instance: LikeWidgetComponent = null;
   beforeEach(async () => {
     await TestBed.configureTestingModule({ //configura um modulo de testes para o componente em questao
       imports: [LikeWidgetModule]
     }).compileComponents() //compila o componente sendo testado, resolvendo seu template, css e etc
 
     fixture = TestBed.createComponent(LikeWidgetComponent); //Este metodo retorna um ComponentFixture<LikeWidgetComponent>
+    instance = fixture.componentInstance;
   })
 
   it('It should create component', () => {
-    const instance = fixture.componentInstance;
     expect(instance).toBeTruthy();
   })
 
   it('Should auto-generate ID when id input property is missing', () => {
-    const instance = fixture.componentInstance;
     fixture.detectChanges(); //dispara o mecanismo de change detection e dispara o lifecicle hook ngOnInit do componente
     expect(instance.id).toBeTruthy();
+  })
+
+  it('Should NOT auto-generate ID when id input property is present', () => {
+    const someId = 'someId';
+    instance.id = someId;
+    fixture.detectChanges(); //dispara o mecanismo de change detection e dispara o lifecicle hook ngOnInit do componente
+    expect(instance.id).toBe(someId);
   })
 })
